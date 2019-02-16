5dfe8c55b0eac58d1bd2d2c99a876e402dbfdfb2
Nathan <nfrenette@carmedia2p0.com>
2019-02-16 12:56:09 -0700

    sketch of algorithm complete

--- beacon.py
+++ beacon.py
@@ -1,11 +1,14 @@
 import validator_pool
+import fuzzer
 
 class Beacon(object):
     def __init__(self, address):
         self.address = address
         self.validator_pool = validator_pool.ValidatorPool()
         self.stakes = []
-        self.entropy = fuzzer.fuzzy_string()
+        self.revealed_entropy = fuzzer.fuzzy_string()
+        self.proposers = []
+        self.proposal_hashes = {}
 
     def stake(self, validator):
         if(validator.address.balance<32):
@@ -15,6 +18,17 @@ class Beacon(object):
         validator.address.balance-=32
         return True
 
-    def request_proposal(self):
-        # get random validator
+    def request_proposal(self, validator):
         # ask random validator to propose a block
+        if(validator.propose()):
+            entropy = validator.get_entropy()
+            if(self.propsal_hashes[validator.address] == str(hash((entropy)))):
+                self.revealed_entropy = self.revealed_entropy^validator.get_entropy()
+            else:
+                pass #will include punishment here
+
+    def reset_proposals(self):
+        self.propsers = []
+
+    def request_proposal_hash(self, validator):
+        self.proposal_hashes[validator.address] = (validator.get_entropy_hash())
--- run.py
+++ run.py
@@ -1,10 +1,12 @@
 import fuzzer
 import Crypto
 from Crypto.PublicKey import RSA
+import random
 
 #constants
 SIMULATION_EPOCHS = 20
-
+AMAX = 10
+EPOCH = 0;
 # create 2048 bit RSA modulus using a secure ceremony
 key = RSA.generate(2048)
 N = key.n # N is used for VDF creation
@@ -12,10 +14,8 @@ N = key.n # N is used for VDF creation
 print(key.n)
 print(key.n.bit_length())
 
-def randao_mix():
-    #STUB
-
-def vdf_calc():
+def vdf_calc(val):
+    return val;
     #STUB
 
 
@@ -30,15 +30,19 @@ for validator in validators:
     beacon.stake(validator)
 
 #set up epoch list - each list contains the most up-to-date information for that epoch
-epoch_states = []
-
+epoch_states = a = [None] * (AMAX + SIMULATION_EPOCHS)
+for x in range(AMAX):
+    epoch_states[x] = fuzzer.fuzzy_string()
 
 for i in range(SIMULATION_EPOCHS):
+    random.seed(hash(epoch_states[i]))
+    random.shuffle(validators)
     #In each epoch, first do the randao mixing, and then calculate the vdf
-    randao_out = randao_mix()
+    for j in beacon.proposers:
+        beacon.request_proposal(validators[j])
+    epoch_states[i+AMAX] = (vdf_calc(beacon.revealed_entropy))
+    beacon.reset_proposals()
+    for j in range(128):
+        beacon.request_proposal_hash(validators[j])
 
-    for i in range(128):
-        beacon.request_proposal()
-    vdf_calc(beacon.entropy) #evaluator
-    #VDF output is used to select proposers
 
