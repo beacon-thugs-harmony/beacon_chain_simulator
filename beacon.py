235a999c386431a7ebd884c47e0a82a9f3882cb9
Nathan <nfrenette@carmedia2p0.com>
2019-02-16 01:05:44 -0500

    closing time

--- beacon.py
+++ beacon.py
@@ -1,15 +1,20 @@
 import validator_pool
-import stake
 
 class Beacon(object):
     def __init__(self, address):
         self.address = address
         self.validator_pool = validator_pool.ValidatorPool()
         self.stakes = []
+        self.entropy = fuzzer.fuzzy_string()
 
     def stake(self, validator):
-    	if(validator.address.balance<32):
-    		return False;
-    	self.validator_pool.add(validator);
-    	self.stakes.append(stake.Stake(self.address,validator.address));
-    	return True;
+        if(validator.address.balance<32):
+            return False
+        self.validator_pool.add(validator)
+        self.address.balance +=32
+        validator.address.balance-=32
+        return True
+
+    def request_proposal(self):
+        # get random validator
+        # ask random validator to propose a block
--- stake.py
+++
@@ -1,6 +0,0 @@
-class Stake(object):
-    def __init__(self, beacon_address, validator_address):
-        beacon_address.balance = beacon_address.balance+32;
-        validator_address.balance = validator_address.balance-32;
-        self.beacon_address = beacon_address
-        self.validator_address = validator_address
